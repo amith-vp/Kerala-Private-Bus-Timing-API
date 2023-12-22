463ae6cae932502d88cc35d84934c0b0d6515675
Amith V Purushothaman <66194856+amith-vp@users.noreply.github.com>
2023-12-22 21:16:26 +0530

    Update routes.rb
    search endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :schedules, only: [:index]
-      resources :test, only: [:index]
+      resources :search, only: [:index]
     end
   end
 end
