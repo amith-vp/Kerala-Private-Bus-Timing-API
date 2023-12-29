e02a79b64d077ac09733eeeb449967d23b7a7051
Amith V Purushothaman <66194856+amith-vp@users.noreply.github.com>
2023-12-29 11:02:52 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :schedules, only: [:index]
+      get 'schedules/:departure/:destination', to: 'schedules#index'
       resources :search, only: [:index]
     end
   end
